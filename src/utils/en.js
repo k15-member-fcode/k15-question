e34eec2d16015c184ffd533e7aa7e7c16bede62a
=thinhtpt <=tthinh2128@gmail.com>
2019-08-22 12:58:16 +0700

    K15 question form

---
+++ src/utils/en.js
@@ -0,0 +1,34 @@
+var titleForm = 'K15 Question Form';
+var titleNote = 'Notes:';
+var noteList = [
+  'Respecting to members of F-Code',
+  'Having a clear content related to F-Code or programing majors at school',
+  'Using appropriate languages: English or accented Vietnamese',
+  'Being able to ask more than one question',
+]
+var formList = [
+  {
+      id: 1,
+      label: 'Name',
+      placeholder: 'Input your name',
+      errMessage: 'Please input your name!'
+  },
+  {
+      id: 2,
+      label: 'Student ID',
+      placeholder: 'Input your student ID',
+      errMessage: 'Please input your student ID!'
+  },
+  {
+      id: 3,
+      label: 'Your Question',
+      placeholder: '',
+      errMessage: 'Please input your question!'
+  },
+]
+var submitBtn = 'Send';
+var notiSucess = {
+  title: 'Notification',
+  content: 'Your question has been sent successfully! We will answer you as soon as possible.',
+  btn: 'Confirm',
+}
---
+++ src/utils/vi.js
@@ -0,0 +1,34 @@
+var titleForm = 'Biểu Mẫu Đặt Câu Hỏi K15';
+var titleNote = 'Chú ý:';
+var noteList = [
+  'Tôn trọng các thành viên F-Code',
+  'Có nội dung rõ ràng và liên quan đến câu lạc bộ hoặc các ngành lập trình ở trường',
+  'Sử dụng ngôn ngữ cho phép: tiếng Anh hoặc tiếng Việt có dấu',
+  'Có thể hỏi nhiều hơn một câu hỏi',
+]
+var formList = [
+  {
+      id: 1,
+      label: 'Họ và tên',
+      placeholder: 'Nhập vào họ tên của bạn',
+      errMessage: 'Hãy nhập tên của bạn!'
+  },
+  {
+      id: 2,
+      label: 'MSSV',
+      placeholder: 'Nhập vào mã sinh viên của bạn',
+      errMessage: 'Hãy nhập mã sinh viên của bạn!'
+  },
+  {
+      id: 3,
+      label: 'Câu hỏi của bạn',
+      placeholder: '',
+      errMessage: 'Hãy nhập câu hỏi của bạn!'
+  },
+]
+var submitBtn = 'Gửi';
+var notiSucess = {
+  title: 'Thông báo',
+  content: 'Câu hỏi của bạn đã được gửi thành công! Chúng tôi sẽ trả lời bạn trong thời gian sớm nhất',
+  btn: 'Xác nhận',
+}
