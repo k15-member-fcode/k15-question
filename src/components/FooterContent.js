b773f34dd210c9d8c535728db018e8846702c51d
thinhtpt2000 <tthinh2128@gmail.com>
2019-08-26 17:47:10 +0700

    Add link fanpage F-Code

--- src/components/FooterContent.js
+++ src/components/FooterContent.js
@@ -4,14 +4,11 @@ import React from "react";
 function FooterContent(props) {
   return (
     <div className="Footer">
-      <div className="footerFanpage">
-        <iframe title="fanpage F-Code" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffcodefpt%2F&tabs&width=340&height=260&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=2323494424561673"></iframe>
-      </div>
-      <div className="footerAuthor">
         <span> {props.footerContent[0]} </span> <br />
         <span> {props.footerContent[1]} </span> <br />
-        <span> {props.footerContent[2]} </span>{" "}
-      </div>
+        <a href="https://www.facebook.com/fcodefpt/" target="_blank" rel="link fanpage F-Code">
+          <span> {props.footerContent[2]} </span>{" "}
+        </a>
     </div>
   );
 }
