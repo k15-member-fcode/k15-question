6011548ad6c3beaf38c17e7850a54ee7e0b24a1b
thinhtpt2000 <tthinh2128@gmail.com>
2019-08-28 21:18:32 +0700

    fix button export

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import QuestionPage from "./QuestionPage";
+import AdminPage from "./AdminPage";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-          <Route path="/" component={QuestionPage} />
+        <Switch>
+          <Route exact path="/" component={QuestionPage} />
+          <Route path="/admin" component={AdminPage} />
+        </Switch>
       </div>
     </Router>
   );
